0cbd2ea4f6fddd9d261bdea0589fb5776816d0bb
Mohamed Slimani <slimani.m1993@gmail.com>
2025-11-02 12:05:58 +0100

    Initial commit

---
+++ resources/js/layouts/app/AppHeaderLayout.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+import AppContent from '@/components/kit/AppContent.vue';
+import AppHeader from '@/components/kit/AppHeader.vue';
+import AppShell from '@/components/kit/AppShell.vue';
+import type { BreadcrumbItemType } from '@/types';
+
+interface Props {
+    breadcrumbs?: BreadcrumbItemType[];
+}
+
+withDefaults(defineProps<Props>(), {
+    breadcrumbs: () => [],
+});
+</script>
+
+<template>
+    <AppShell class="flex-col">
+        <AppHeader :breadcrumbs="breadcrumbs" />
+        <AppContent>
+            <slot />
+        </AppContent>
+    </AppShell>
+</template>
